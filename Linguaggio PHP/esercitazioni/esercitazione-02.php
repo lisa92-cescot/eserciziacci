120abe8596371b7187803d6732f220ab0df56f58
lisa <lisa.casaburo@outlook.it>
2025-11-24 10:15:32 +0100

    r

--- Linguaggio PHP/esercitazioni/esercitazione-02.php
+++ Linguaggio PHP/esercitazioni/esercitazione-02.php
@@ -1,5 +1,20 @@
 <?php
 
-$valori = [1, 3, 5, 2, 5, 3, 9, 4, 7]
 
-if $valori = [1, 3, 5, 2, 3, 9, 4, 7]
+$valori = [1, 3, 5, 2, 5, 3, 9, 4, 7];
+$massimo = 0;
+
+for ($i = 0; $i < count($valori); $i++) :
+    if ($valori [$i] > $massimo) :
+        $massimo = $valori[$i];
+    endif;
+endfor
+
+?>
+    <div>
+        <h2>Esercizio 01</h2>
+        <p>
+            Il valore maggiore è: </strong> <?php echo $massimo; ?>
+        </p>
+    </div>
+
