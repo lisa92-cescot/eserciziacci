7b679908d5068a5a58a873b190fc72051bf82364
lisa <lisa.casaburo@outlook.it>
2025-11-26 10:39:27 +0100

    java

---
+++ Linguaggio JS/script.js
@@ -0,0 +1,62 @@
+//*Commento multilinea*/
+//Commento in linea
+
+//Stampa in console
+console.log("Ciao, mondo!");
+//Popup un messaggio
+
+//window.alert("Benvenuto!");
+
+//Mostra un popup che restituisce true o false
+//window.log(window.confirm("Sei sicuro?"));
+
+//Mostra un popup che chiede un dato
+//window.prompt("Inserisci il tuo nome:");
+
+
+//Dichiarazione di variabile
+var stringa1 = "Ciao, mondo!"; //Variabile globale
+let stringa2 = "Ciao mondo!"; //Variabile locale
+const costante = "Ciao mondo!"; //Variabile costante
+
+
+// Stringhe
+var stringa ="Insieme di caratteri";
+console.log(stringa[2]); //String
+
+//Interi -> int
+var intero = 1;
+console.log(typeof intero); //Number
+
+//Decimali -> float
+var decimale = 1.2;
+console.log(typeof decimale); //Number
+
+//Booleani -> true o false
+var bool = true;
+console.log(typeof bool); //Boolean
+
+//Array -> insieme di valori
+var array = [1, 2, 3, 4, 5, 6];
+console.log(typeof array); 
+
+// Oggetto
+var object = {
+    nome: "Mario",
+    cognome: "Rossi",
+    eta: 30
+};
+console.log(typeof object); //Object
+
+//Operatori matematici
+var a =1;
+var b =2;
+
+console.log(a + b); //Addizione
+console.log(a - b); //Sottrazione
+console.log(a * b); //Moltiplicazione
+console.log(a / b); //Divisione
+console.log(a % b); //Modulo
+
+console.log(a++); // Leggo e poi Incremento
+console.log(++a); // Incremento e poi leggo
